ee43b434d13ef8b80961711be02aadd934608c79
unknown <mdrakibulhoque83@gmail.com>
2021-10-04 23:34:18 +0600

    instructor update

--- src/Components/Instructor/Instructor.js
+++ src/Components/Instructor/Instructor.js
@@ -1,15 +1,18 @@
 
 import Footer from '../Footer/Footer';
 import MenuBar from '../MenuBar/MenuBar';
+import "./Instructor.css";
 
 const Instructor = () => {
 
     return (
         <div>
             <MenuBar></MenuBar>
+
             <div className="instructor-container">
-                <h1>Our Instructors</h1>
+                
             </div>
+
             <Footer></Footer>
         </div>
     );
