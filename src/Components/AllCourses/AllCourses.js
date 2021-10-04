6357d01a916b2e5b3bc20123f16b2403db6f4555
unknown <mdrakibulhoque83@gmail.com>
2021-10-04 23:40:02 +0600

    code organized

--- src/Components/AllCourses/AllCourses.js
+++ src/Components/AllCourses/AllCourses.js
@@ -24,7 +24,7 @@ const AllCourses = () => {
                             key={scourse.id}
                             scourse={scourse}
                         >
-                            
+
                         </AllCourseShow>
                     )
                 }
